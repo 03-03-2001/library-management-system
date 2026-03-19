d518cb4077c79dbccdf7b1a5d483b076a2e51d4c
03-03-2001 <rohitvkamble0301@gmail.com>
2026-03-19 20:48:19 +0530

    initial set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8081;
+
+app.use(express.json());
+
+app.get((req,res)=>{
+    res.status(200).json({
+        message: 'Home Page'
+    })
+})
+
